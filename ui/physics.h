81f53ee45cf2be0fae3a627cc676b0aaefc48ed3
amir@amir <arsalan_system_computer@yahoo.com>
2020-12-10 13:08:54 -0500

    physics

---
+++ ui/physics.h
@@ -0,0 +1,42 @@
+#ifndef PHYSICS_H
+#define PHYSICS_H
+
+#include "vector"
+#include "memory"
+#include "glm/glm.hpp"            // glm::vec*, mat*, and basic glm functions
+
+// Rigid Physics
+struct Sphere{
+    glm::vec3 position;
+    glm::vec3 velocity;
+    glm::vec3 force;
+    glm::vec3 acceleration;
+    float mass;
+};
+
+struct Plane{
+    glm::vec3 position;
+    glm::vec3 normal;
+};
+
+class Physics{
+
+public:
+    Physics(int fps);
+    virtual ~Physics();
+
+    // Rigid physics
+    void computeForce(glm::vec3& forceVec, float mass);
+    void updateAcceleration(glm::vec3& acceleration, glm::vec3& force, float mass);
+    void updateVelocity(glm::vec3 &velocity, glm::vec3 &acceleration);
+    void updatePosition(glm::vec3 &position, glm::vec3 &velocity);
+
+    void collisionDetection(Sphere& sphere, std::vector<Plane>& walls);
+    void runPhysics(std::vector<Sphere>& spheres);
+
+    glm::vec3 m_g;
+    int m_fps;
+
+};
+
+#endif // PHYSICS_H
